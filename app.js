7db9d2cb88c9a70d9546ac5f219f83c239f99542
kaymoore <kaymoore@macbook>
2022-04-24 09:20:58 -0400

    Add range slider that updates nxn grid layout

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 // sets up 16 x 16 board
-function setupGameBoard(){
-    let numRows = 16;
-    let numCols = 16;
+function setupGameBoard(n = 16){
     let gameBoard = document.querySelector(".gameBoard");
 
-    for (let i = 0; i < numRows; i++){
+    for (let i = 0; i < n; i++){
         const newRow = document.createElement("div");
         newRow.classList.add("gameRow");
-        for (let j = 0; j < numCols; j++){
+        for (let j = 0; j < n; j++){
             const newCell = document.createElement("div");
             newCell.classList.add("gameCell");
+            newCell.addEventListener('mousemove', cellHovered)
             newRow.appendChild(newCell);
         }
         gameBoard.appendChild(newRow);
+    }
+}
+
+function clearGameBoard(){
+    let gameBoard = document.querySelector(".gameBoard");
+    while (gameBoard.firstChild){
+        gameBoard.removeChild(gameBoard.firstChild);
     }
 }
 
@@ -29,8 +35,14 @@ function cellHovered(e){
 
 setupGameBoard();
 
-let gameCells = document.querySelectorAll(".gameCell");
-gameCells.forEach(cell => cell.addEventListener('mousemove', cellHovered));
-
 let resetBtn = document.querySelector(".resetBtn");
 resetBtn.addEventListener('click', resetGameBoard);
+
+// Updates grid layout based on range slider
+let boardSizeSlider = document.getElementById("boardSize");
+let boardSizeOutput = document.getElementById("boardSizeValue");
+boardSizeSlider.oninput = function(){
+    boardSizeOutput.textContent = `${this.value} X ${this.value}`;
+    clearGameBoard();
+    setupGameBoard(this.value);
+}
